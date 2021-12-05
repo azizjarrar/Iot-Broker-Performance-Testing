ddabb21ef482e6410cebe5f64d6269282cd0a819
aziz jarrar <azizjarrar@gmail.com>
2021-12-05 14:49:31 +0100

    finish

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ let CountMessageRecived=0// count recived messages
 let LeastAndMostTime=[];//countain all packets time to recive data
 let LeastAndMostTimeString=[];// container all payloads
 var sendDataEveryS // setInterval Function run every 1s
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
 /******************************************************/
 /********generate unique id for each user**************/
@@ -16,6 +20,7 @@ const genereId=()=>{
 /********send data to topic**************/
 /****************************************/
 const  PublishData=(string,clientId,client,topic,quality)=>{
+  CountMessageSent++ 
   let currentTime=Date.now()
   client.publish(topic, `client_id ${clientId} String = ${string} currentTimeOfSendingData=${currentTime}`   , { qos: quality,retain: true }, (error) => {
     if (error) {
@@ -27,6 +32,7 @@ const  PublishData=(string,clientId,client,topic,quality)=>{
 /********each client connexion data with publish n times*****/
 /************************************************************/
 const clientConnexion=(clientId,pushblishCount,string,topics,connectUrl,quality)=>{
+
     const client = mqtt.connect(connectUrl, {
         clientId,
         clean: true,
@@ -38,13 +44,12 @@ const clientConnexion=(clientId,pushblishCount,string,topics,connectUrl,quality)
 
     client.on("error",(error)=>{ console.log(error)});
       for(let i = 0 ; i< pushblishCount ;i++){
-        CountMessageSent++ 
         for(let t = 0; t < topics.length;t++){
           PublishData(string,clientId,client,topics[t],quality)
         }
       }
 }
-const performanceTest=(repteTime,finishTime,stringLnength,clientNumber,testTimeForEachClient,topics,subscribeName,host,port,quality)=>{
+let performanceTest=(repteTime,finishTime,stringLnength,clientNumber,testTimeForEachClient,topics,subscribeName,host,port,quality)=>{
    connectUrl = `${host}:${port}`
     /**********************************/
     /*********global connexion*********/
@@ -58,7 +63,7 @@ const performanceTest=(repteTime,finishTime,stringLnength,clientNumber,testTimeF
       reconnectPeriod: 1000,
     })
     if(subscribeName!=undefined){
-      client.subscribe([subscribeName],{rh :true}, () => { 
+      client.subscribe([...subscribeName],{rh :true}, () => { 
         console.log(`Subscribe to topic '${subscribeName}'`)
       })
     }
@@ -82,7 +87,7 @@ const performanceTest=(repteTime,finishTime,stringLnength,clientNumber,testTimeF
         LeastAndMostTime.push(RecivedTime - parseInt(stringBuf.substr(-13)))
         LeastAndMostTimeString.push(stringBuf)
         //console.log('Received Message:',stringBuf,"topic Name :",topic , " RecivedTime : "  +RecivedTime  + " time to recive =" + (RecivedTime - parseInt(stringBuf.substr(-13)))+"ms")
-        console.log(stringBuf.slice(0,27),"topic Name :",topic , " RecivedTime : "  +RecivedTime  + " time to recive =" + (RecivedTime - parseInt(stringBuf.substr(-13)))+"ms")
+        console.log(stringBuf.slice(0,27),"topic Name :",topic ,   " time to recive =" + (RecivedTime - parseInt(stringBuf.substr(-13)))+"ms")
         }
       })
     /**************************************************************/
@@ -93,27 +98,53 @@ const performanceTest=(repteTime,finishTime,stringLnength,clientNumber,testTimeF
     console.log("succes ratio",((CountMessageRecived)/CountMessageSent)*100 + "%")
     console.log("CountMessageRecived :", CountMessageRecived)
     console.log("CountMessageSent :", CountMessageSent)
-    var min = Math.min(...LeastAndMostTime.slice(1,-1));
-    var max = Math.max(...LeastAndMostTime.slice(1,-1));
+    console.log(LeastAndMostTime)
+    var min = Math.min(...LeastAndMostTime);
+    var max = Math.max(...LeastAndMostTime);
     console.log("min Time: " + min ,"s", "max Time:" + max,"s")
     console.log("recived messages in :"+finishTime/1000+"s")
     CountMessageRecived=0
     CountMessageSent=0
     clearInterval(sendDataEveryS)
+    readline.close()
+
 }, finishTime);
 }
 
 
 
-("repteTime","finishTime","stringSize","clientNumber","number of publish for topic","topics []","subscribeName","host","port","quality")
+("repteTime","finishTime","stringSize","clientNumber","number of publish for topic","topics []","subscribeName []","host","port","quality")
 
-var topics = [];
-var topicsNumber=1
-for (var i = 1; i <= topicsNumber; i++) {
-  topics.push("topic"+i);
-}
+
 //limit packet size 1mo i cant send more then 1023999char
 // can send 1023999
 //mqtt://broker.emqx.io:1883
 //"mqtts://j4f805b0.eu-central-1.emqx.cloud","15296"
-performanceTest(1000,1900,1023999,2,1,topics,"topic1","mqtt://broker.emqx.io","1883",1)
+
+readline.question(`repteTime  each ms ? :`, repteTime => {
+  readline.question(`finishTime  with ms ? :`, finishTime => {
+    readline.question(`stringSize ? :`, stringSize => {
+      readline.question(`clientNumber ? :`, clientNumber => {
+        readline.question(`number of publish for  each topic ? :`, numberofpublishfortopic => {
+          readline.question(`topics number ?`, topicsNumber => {
+            var topics = [];
+            for (var i = 1; i <= topicsNumber; i++) {
+              topics.push("topic"+i);
+            }
+              readline.question(`qualityofservice : `, qualityofservice => {
+                performanceTest(repteTime,finishTime,stringSize,clientNumber,numberofpublishfortopic,topics,topics,"mqtts://j4f805b0.eu-central-1.emqx.cloud","15296",qualityofservice-0)
+              })
+          })
+        })
+      })
+    })
+  })
+})
+/*
+var topics = [];
+var topicsNumber=10
+for (var i = 1; i <= topicsNumber; i++) {
+  topics.push("topic"+i);
+}
+performanceTest(1000,1900,10,2,1,topics,topics,"mqtts://j4f805b0.eu-central-1.emqx.cloud","15296",1)
+*/
